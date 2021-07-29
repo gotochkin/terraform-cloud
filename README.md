ae02e3d9cfafba0ba38f490fae030c3feec7af46
Gleb Otochkin <gleb.otochkin@gmail.com>
2021-07-29 11:34:56 -0400

    added main.tf for demo01

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # terraform-cloud
-For terraform cloud configurations
+For terraform cloud demo configurations and tests
+
+modules-demo - Terraform modules tests
+    modules - directory for modules
+    demo01 - simplest module demo 
---
+++ modules-demo/demo01/main.tf
@@ -0,0 +1,29 @@
+#    Created by Gleb Otochkin 2021-01-24  
+# Provider and authentication details
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+
+data "oci_identity_compartment" "database_test_compartment" {
+    #Required
+    id = var.compartment_ocid
+}
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
+}
+
+# Modules declaration 
+
+module "security_lists" {
+  source = "../modules/security_lists"
+  vcn_id = oci_core_vcn.moduletest_vcn.id
+  compartment_ocid = var.compartment_ocid
+  security_list_id = oci_core_vcn.moduletest_vcn.default_security_list_id
+}
+
